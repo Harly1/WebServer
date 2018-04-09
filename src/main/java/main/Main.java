4178c8fab8c2ecde31923801a7799f89a0369722
harly <harly1@yandex.ru>
2018-04-09 17:44:51 +0300

    Area ready

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,18 +5,18 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 
-        public class Main {
-            public static void main(String[] args) throws Exception {
-                AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+ public class Main {
+     public static void main(String[] args) throws Exception {
+         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
-                ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-                context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
-                Server server = new Server(8080);
-                server.setHandler(context);
+         Server server = new Server(8080);
+         server.setHandler(context);
 
-                server.start();
-                server.join();
-            }
-        }
+         server.start();
+         server.join();
+     }
+ }
 
